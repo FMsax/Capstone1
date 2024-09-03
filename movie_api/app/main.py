69986690c4a970e7db3642f802f60b4c9c4a2e3d
oluwaferanmiAsiwaju <93679363+FMsax@users.noreply.github.com>
2024-09-03 09:38:17 +0100

    main.py

--- movie_api/app/main.py
+++ movie_api/app/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from app.routers import movies, users, comments, ratings
 from app.database import engine
 from app import models
+from uvicorn import run
+
+
 
 # Create the database tables
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Your routes and logic here
+
+if __name__ == "__main__":
+    run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 app.include_router(users.router)
 app.include_router(movies.router)
